868d1c027a7b476aa3f214f40dac9e10cfb888bc
Deniz Ugurlu <mail@denizugurlu.de>
2016-02-14 22:17:08 +0100

    starting to work on game experience; todo: split power-on and start game logic

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,55 +2,75 @@ var game = {
   quadrants: {
     sq1: {
       colorActive: '#F00',
-      colorNormal: '#C00',
+      colorNormal: '#B10000',
     },
     sq2: {
       colorActive: '#0F0',
-      colorNormal: '#0C0',
+      colorNormal: '#00B100',
     },
     sq3: {
       colorActive: '#00F',
-      colorNormal: '#00C',
+      colorNormal: '#0000B1',
     },
     sq4: {
       colorActive: '#FF0',
-      colorNormal: '#CC0',
+      colorNormal: '#B1B100',
     }
   },
+  sequence: [],
 	running: false,
 	strict: false,
 	init: function() {
     this.cacheDom();
     this.bindEvents();
   },
-  toggleRunning: function() {
-    // TODO
-	  if (this.running) {
-      this.resetGame();
-	  } else {
-      this.startGame();
-	  }
-	  
-	  this.running = !this.running;
-	},
 	resetGame: function() {
     // TODO
     this.$switchPosition.css('left', '2px');
-    this.$button.removeClass('button-active');
+    this.deactivateButtons();
     this.$display.css('visibility', 'hidden');
+    this.sequence = [];
   },
   startGame: function() {
-    // TODO
+    // TODO separate 'power up' and 'start game'
     this.$switchPosition.css('left', '28px');
-    this.$button.addClass('button-active');
+    this.activateButtons();
     this.$display.css('visibility', 'visible');
+    this.deactivateButtons();
+    setTimeout(this.play.bind(this), 1000);
+  },
+  play: function() {
+    // get new random quadrant to play
+    this.deactivateButtons();
+    var newQuadrant = 'sq' + Math.floor(Math.random() * 4);
+    this.sequence.push(newQuadrant);
+    this.playSequence();
+    this.activateButtons();
+    this.checkUserSequence(); // TODO
+  },
+  deactivateButtons: function() {
+    this.$button.removeClass('button-active');
+    this.$quadrant.addClass('unclickable');
+  },
+  activateButtons: function() {
+    this.$button.addClass('button-active');
+    this.$quadrant.removeClass('unclickable');
+  },
+  playSequence: function() {
+    for(var i in this.sequence) {
+      setTimeout(this.activateQuadrant.bind(this), i * 1000, this.sequence[i]);
+    }
+  },
+  checkUserSequence: function() {
+
   },
 	activateQuadrant: function(event) {
     if(this.running === false) {
       return;
     }
-    var id = event.currentTarget.getAttribute('id');
+    var id = (typeof event === 'string') ? event : event.currentTarget.getAttribute('id');
     // highlight the button
+    this.deactivateButtons();
 	  this.quadrants[id]['$element'].css('background-color', this.quadrants[id]['colorActive']);
     // play audio
     this.quadrants[id]['audio'].currentTime = 0;
@@ -58,7 +78,10 @@ var game = {
     setTimeout(this.deactivateQuadrant.bind(this), 1000, id);
 	},
   deactivateQuadrant: function(id) {
+    this.quadrants[id]['audio'].pause();
+    this.quadrants[id]['audio'].currentTime = 0;
     this.quadrants[id]['$element'].css('background-color', this.quadrants[id]['colorNormal']);
+    this.activateButtons();
   },
   cacheDom: function() {
     this.$quadrant = $('.quadrant');
@@ -77,11 +100,9 @@ var game = {
     this.$switch.on('click', this.toggleSwitch.bind(this));
   },
   toggleSwitch: function() {
-    // TODO
     if(this.running) {
       this.resetGame();
     } else {
-      // start game
       this.startGame();
     }
     this.running = !this.running;
